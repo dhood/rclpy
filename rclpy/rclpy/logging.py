49cc6cf4b95091f9aaa51015fde2ec53ed43afc0
dhood <dhood@users.noreply.github.com>
2017-08-22 16:52:29 -0700

    wip log macros with kwargs

--- rclpy/rclpy/logging.py
+++ rclpy/rclpy/logging.py
@@ -17,18 +17,8 @@ from enum import IntEnum
 
 from rclpy.impl.implementation_singleton import rclpy_logging_implementation as _rclpy_logging
 
-
-def initialize():
-    return _rclpy_logging.rclpy_logging_initialize()
-
-
-def get_severity_threshold():
-    return _rclpy_logging.rclpy_logging_get_severity_threshold()
-
-
-def set_severity_threshold(severity):
-    assert isinstance(severity, LoggingSeverity) or isinstance(severity, int)
-    return _rclpy_logging.rclpy_logging_set_severity_threshold(severity)
+import rclpy.rcutils
+from rclpy.rcutils import feature_combinations
 
 
 class LoggingSeverity(IntEnum):
@@ -43,3 +33,47 @@ class LoggingSeverity(IntEnum):
     RCLPY_LOG_SEVERITY_WARN = 2
     RCLPY_LOG_SEVERITY_ERROR = 3
     RCLPY_LOG_SEVERITY_FATAL = 4
+
+
+def initialize():
+    return _rclpy_logging.rclpy_logging_initialize()
+
+
+def get_severity_threshold():
+    return _rclpy_logging.rclpy_logging_get_severity_threshold()
+
+
+def set_severity_threshold(severity):
+    assert isinstance(severity, LoggingSeverity) or isinstance(severity, int)
+    return _rclpy_logging.rclpy_logging_set_severity_threshold(severity)
+
+
+def log(
+        message, severity=LoggingSeverity.RCLPY_LOG_SEVERITY_INFO, *,
+        name=None, once=False, throttle_duration=None, throttle_time_source_type=None):
+    suffix = ''
+    # Build up the suffix
+    if throttle_duration or throttle_time_source_type:
+        suffix += '_THROTTLE'
+    if once:
+        suffix += '_ONCE'
+    if name:
+        suffix += '_NAMED'
+    if suffix not in rclpy.rcutils.feature_combinations:
+        raise AttributeError('invalid logging combination')
+    # Get the ordered dict for that suffix (maintaining ordering is important)
+    args = rclpy.rcutils.get_macro_parameters(suffix)
+    if throttle_duration or throttle_time_source_type:
+        args['duration'] = throttle_duration
+        args['time_source_type'] = throttle_time_source_type
+    if once:
+        args['name'] = name
+    if name:
+        args['name'] = name
+    required_params = rclpy.rcutils.get_macro_parameters(suffix)
+    for p in required_params:
+        if p not in args.keys() or args[p] is None:
+            raise RuntimeError('required parameter {0} not specified but required for logging {1}'.format(p, suffix))
+    print(suffix)
+    print(args)
+    return
