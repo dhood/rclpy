bd2db52493d9d57036c2138a0b6b5bfea112287a
dhood <dhood@users.noreply.github.com>
2017-08-24 13:46:12 -0700

    ignore False keywords, use the severity

--- rclpy/rclpy/logging.py
+++ rclpy/rclpy/logging.py
@@ -32,6 +32,18 @@ class LoggingSeverity(IntEnum):
     RCLPY_LOG_SEVERITY_ERROR = 3
     RCLPY_LOG_SEVERITY_FATAL = 4
 
+    def short_form(self):
+        if self.name == 'RCLPY_LOG_SEVERITY_DEBUG':
+            return 'debug'
+        if self.name == 'RCLPY_LOG_SEVERITY_INFO':
+            return 'info'
+        if self.name == 'RCLPY_LOG_SEVERITY_WARN':
+            return 'warn'
+        if self.name == 'RCLPY_LOG_SEVERITY_ERROR':
+            return 'error'
+        if self.name == 'RCLPY_LOG_SEVERITY_FATAL':
+            return 'fatal'
+
 
 def initialize():
     return _rclpy_logging.rclpy_logging_initialize()
@@ -68,27 +80,32 @@ def logfatal(message):
 
 def log(message, severity, **kwargs):
     assert isinstance(severity, LoggingSeverity) or isinstance(severity, int)
+    severity = LoggingSeverity(severity)
 
     # Build up the suffix
     suffix = ''
-    if 'skipfirst' in kwargs:
+    if kwargs.get('skipfirst'):
         suffix += '_SKIPFIRST'
-    if 'duration' in kwargs or 'time_source_type' in kwargs:
+    if kwargs.get('duration') or kwargs.get('time_source_type'):
         suffix += '_THROTTLE'
-    if 'once' in kwargs:
+    if kwargs.get('once'):
         suffix += '_ONCE'
-    if 'name' in kwargs:
+    if kwargs.get('name'):
         suffix += '_NAMED'
 
     if suffix not in supported_feature_combinations:
         raise AttributeError('invalid combination of logging features')
 
-    # Get the ordered dict for that suffix (maintaining ordering is important)
     required_params = get_macro_parameters(suffix)
+    # Copy only the required arguments into a minimal dictionary
+    # TODO(dhood): make c functions ignore unnecessary keyword arguments
+    params = {}
     for p in required_params:
-        if p not in kwargs:
+        try:
+            params[p] = kwargs[p]
+        except:
             raise RuntimeError(
-                'required parameter {0} not specified '
-                'but required for logging feature {1}'.format(p, suffix))
-    f = getattr(_rclpy_logging, 'rclpy_logging_log_' + 'info' + suffix.lower())
-    return f(message, **kwargs)
+                'required parameter "{0}" not specified '
+                'but required for logging feature "{1}"'.format(p, suffix))
+    f = getattr(_rclpy_logging, 'rclpy_logging_log_' + severity.short_form() + suffix.lower())
+    return f(message, **params)
